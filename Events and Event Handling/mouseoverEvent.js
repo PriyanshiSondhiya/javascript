8a853c70cc12d9caad0ce352b844e2e0d1264230
Priyanshi Sondhiya <priyanshisondhiya8@gmail.com>
2026-07-07 12:08:40 +0530

    live character counter

---
+++ Events and Event Handling/mouseoverEvent.js
@@ -0,0 +1,57 @@
+let abcd = document.querySelector("#abcd");
+
+window.addEventListener("mousemove", function (dets) {
+  abcd.style.top = dets.clientY + "px";
+  abcd.style.left = dets.clientX + "px";
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
